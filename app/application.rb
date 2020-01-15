1e3d36bd877671c0b36f309af2ccc9a046d31c97
Deanna Kelley <deanna.georges@gmail.com>
2020-01-15 16:54:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    if time.now >= 12 
+    if time.now.hour < 12 
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
